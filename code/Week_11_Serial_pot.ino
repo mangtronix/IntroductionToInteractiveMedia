f44be59a2026b5ebdf34c2e77fdef6a1792b45a1
Michael Ang <contactmang@gmail.com>
2025-04-15 18:13:24 +0400

    Update Week_11_Serial_pot.ino

--- code/Week_11_Serial_pot.ino
+++ code/Week_11_Serial_pot.ino
@@ -4,27 +4,28 @@
  * Pair with https://editor.p5js.org/mangtronix/sketches/vQzE2Yhpj
  * 
  * Inputs:
- *   - A0 - 10k potentiometer connected to 5V and GND
+ *   - A1 - 10k potentiometer connected to 5V and GND
  *
  * If you get "Can't upload" make sure your p5js sketch is stopped.
  * Only one program can access a given serial port at once.
  */ 
 
 void setup() {
-  Serial.begin(9600); // initialize serial communications
+  Serial.begin(57600); // initialize serial communications
 }
  
 void loop() {
   // read the input pin:
-  int potentiometer = analogRead(A0);                  
-  // remap the pot value to fit in 1 byte:
-  int mappedPot = map(potentiometer, 0, 1023, 0, 255); 
-  // print it out the serial port:
-  Serial.write(mappedPot);                             
+  int potentiometer = analogRead(A1);                  
+  // remap the pot value to 0-255:
+  int mappedPotValue = map(potentiometer, 0, 1023, 0, 255); 
+  // print the value to the serial port.
+  // the println function converts the value from e.g. the numeric value 222 to the string "222\n"
+  Serial.println(mappedPotValue);
   // slight delay to stabilize the ADC:
   delay(1);                                            
   
   // Delay so we only send 10 times per second and don't
-  // flood the serial connection
+  // flood the serial connection leading to missed characters on the receiving side
   delay(100);
 }
