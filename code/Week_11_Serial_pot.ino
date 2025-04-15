a09db41dcf3da9857c8c71881d4b959ecb33b98b
Michael Ang <contactmang@gmail.com>
2025-04-15 18:17:16 +0400

    Update Week_11_Serial_pot.ino

--- code/Week_11_Serial_pot.ino
+++ code/Week_11_Serial_pot.ino
@@ -8,10 +8,13 @@
  *
  * If you get "Can't upload" make sure your p5js sketch is stopped.
  * Only one program can access a given serial port at once.
- */ 
+ */
+
+int interval = 100;
+int lastMessageTime = 0;
 
 void setup() {
-  Serial.begin(57600); // initialize serial communications
+  Serial.begin(9600); // initialize serial communications
 }
  
 void loop() {
@@ -27,5 +30,7 @@ void loop() {
   
   // Delay so we only send 10 times per second and don't
   // flood the serial connection leading to missed characters on the receiving side
+  // For a real sketch we would want to use timing rather than a delay, so we can stay responsive
+  // to user inputs on the Arduino side. See https://docs.arduino.cc/built-in-examples/digital/BlinkWithoutDelay
   delay(100);
 }
